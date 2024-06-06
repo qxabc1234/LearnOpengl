5ce9a1253773a8fdd2651204d685d8f233be322d
trouger <trouger@126.com>
2024-06-06 14:17:43 +0800

    add normal map

--- LearnOpengl/model_loading.fs
+++ LearnOpengl/model_loading.fs
@@ -1,36 +1,44 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec2 TexCoords;
-in vec3 Normal;  
-in vec3 FragPos;  
+in VS_OUT {
+    vec3 FragPos;
+    vec2 TexCoords;
+    vec3 TangentLightPos;
+    vec3 TangentViewPos;
+    vec3 TangentFragPos;
+} fs_in;
   
 uniform vec3 lightPos; 
 uniform vec3 viewPos; 
 uniform vec3 lightColor;
 
 uniform sampler2D texture_diffuse1;
+uniform sampler2D texture_normal1;
+uniform sampler2D texture_specular1;
 
 void main()
 {        
     // ambient
     float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
+    vec3 ambient = ambientStrength * lightColor * vec3(texture(texture_diffuse1, fs_in.TexCoords));
   	
     // diffuse 
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
+    vec3 normal = texture(texture_normal1, fs_in.TexCoords).rgb;
+    normal = normalize(normal * 2.0 - 1.0); 
+    vec3 color = texture(texture_diffuse1, fs_in.TexCoords).rgb;
+    vec3 lightDir = normalize(fs_in.TangentLightPos - fs_in.TangentFragPos);
+    float diff = max(dot(lightDir, normal), 0.0);
+    vec3 diffuse = diff * lightColor * color;
     
     // specular
     float specularStrength = 0.5;
-    vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);  
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor;  
+    vec3 viewDir = normalize(fs_in.TangentViewPos - fs_in.TangentFragPos);
+    vec3 reflectDir = reflect(-lightDir, normal);
+    vec3 halfwayDir = normalize(lightDir + viewDir);  
+    float spec = pow(max(dot(normal, halfwayDir), 0.0), 32.0);
+    vec3 specular = specularStrength * spec * lightColor * vec3(texture(texture_specular1, fs_in.TexCoords));
      
-    vec3 objectColor = vec3(texture(texture_diffuse1, TexCoords));
-    vec3 result = (ambient + diffuse + specular) * objectColor;
-    FragColor = vec4(result, 1.0);
+    FragColor = vec4(ambient + diffuse + specular, 1.0);   
+
 }
